4362b34fb98c9857b2fe18e65adcc5ae1d567b52
athul <unni65129@gmail.com>
2025-03-10 16:29:24 +0530

    java

--- model.js
+++ model.js
@@ -9,22 +9,151 @@
 //     console.log(i+"*"+n+"="+s)
 //     i++
 // }
+
+
+
 // for(i=1;i<=10;i++){
 //     s=i*n
 //     console.log(i+"*"+n+"="+s)
 // }
+
+
+// let a=parseInt(prompt("enter the number"))
+// let b=parseInt(prompt("enter the second number"))
+// let i
+// let s=0
+// for(i=1;a<=b;i++)
+// {
+//     let j
+//     for(j=1;j<=10;j++)
+//     {
+//     s=j*a
+//     console.log(j+"*"+a+"="+s)
+//     }
+//     console.log("")
+//     a=a+1
+// }
+
+
+// function fun(name,age){
+//     console.log(name,age)
+// }
+// fun("athul",20)
+
+
+// const data = function(a,b){
+//     console.log(a,b);
+// }
+// data("athul",20)
+
+
+// const data = () => {
+
+//     console.log("hello")
+//     console.log("hello")
+// }
+// data()
+
+
+// let a="welcome"
+// console.log(a.length);
+// console.log(a.charAt);
+// console.log(a.concat(" hello"));
+// console.log(a.endsWith('e'));
+// console.log(a.indexOf(4));
+// console.log(a.padEnd(20,'hi'));
+// console.log(a.padStart(20));
+// let b="                           hi                  "
+// console.log(b.trim());
+// console.log(b.trimEnd());
+// console.log(b.trimStart());
+
+// console.log(a.slice(0,4));
+
+// console.log(a.split('e'));
+// console.log(a.toLocaleLowerCase());
+// console.log(a.toUpperCase());
+// console.log(a.toLowerCase());
+// console.log(a.valueOf());
+
+
+
+// let a=[1,2,3,4,5,6]
+// console.log(a)
+// console.log(a[0]);
+
+
+// console.log(a.length);
+// console.log(a.concat([1,2,3]));
+// console.log(a);
+// a.pop();
+// console.log(a);
+// a.push(['hello'])
+// console.log(a);
+// a.unshift(3)
+// console.log(a);
+
+
+// a.shift();
+// console.log(a);
+// console.log(a.splice(0,3));
+// a.join("hi")
+// console.log(a);
+
+
+
+
+// let a=parseInt(prompt("enter the number"))
+// let b=parseInt(prompt("enter the second number"))
+// let i
+// let s=0
+// for(i=1;a<=b;i++)
+// {
+// s=s+a
+// a=a+1
+// }
+// console.log('sum=',s);
+
+
+
+// let a=parseInt(prompt("enter the number"))
+// let b=parseInt(prompt("enter the second number"))
+// let i
+// let s=0
+// if(a%2!=0)
+//     a=a+1
+// for(i=1;a<=b;i++)
+// {
+// s=s+a
+// a=a+2
+// }
+// console.log("sum of even =",s);
+
+
+
 let a=parseInt(prompt("enter the number"))
 let b=parseInt(prompt("enter the second number"))
 let i
 let s=0
+if(a%2==0)
+    a=a+1
 for(i=1;a<=b;i++)
 {
-    let j
-    for(j=1;j<=10;j++)
-    {
-    s=j*a
-    console.log(j+"*"+a+"="+s)
-    }
-    console.log("")
-    a=a+1
+s=s+a
+a=a+2
 }
+console.log("sum of odd =",s);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
